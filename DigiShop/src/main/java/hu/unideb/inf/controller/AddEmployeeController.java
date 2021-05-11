66e27e2c0e1470186aa085a0fc1e19d72e238f09
Limitsofmyimagination <81773455+Limitsofmyimagination@users.noreply.github.com>
2021-05-11 10:28:06 +0200

    Project1

--- DigiShop/src/main/java/hu/unideb/inf/controller/AddEmployeeController.java
+++ DigiShop/src/main/java/hu/unideb/inf/controller/AddEmployeeController.java
@@ -130,7 +130,7 @@ public class AddEmployeeController implements Initializable  {
         
              try {
  ((JpaEmployeeDAO)aDAO).saveUser(dew);
- JOptionPane.showMessageDialog(frame,"employees has  been created ");
+ JOptionPane.showMessageDialog(frame,"NEW employee has  been added ");
 }
 catch(Exception e) {
   JOptionPane.showMessageDialog(frame,e+"employees has not been created cauz");
